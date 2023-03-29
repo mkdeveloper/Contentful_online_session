387f8beaab4e351ced290dc4b1671ce1b126e343
mkdeveloper <khubaib.mk@gmail.com>
2023-03-29 15:15:51 +0500

    First Commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  env : {
+    CONTENTFUL_SPACE_ID:"ih7h3ygbqvig",
+    CONTENTFUL_ACCESS_KEY: "4GgaUPWvgH2xlIxprcHzH1ogH1PHb9k8rCF-BCUM5sk"
+  },
   experimental: {
     appDir: true,
   },
+ 
 }
 
 module.exports = nextConfig
